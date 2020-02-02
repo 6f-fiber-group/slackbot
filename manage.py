e08ccb5e97f6052fee2de6f787f65a2de286c399
mmfiber <masa.03191996@gmail.com>
2020-02-02 22:48:33 +0900

    for test

--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
 from bot_app import app
 
 if __name__ == "__main__":
-  app.run(host="127.0.0.1", port=5000, debug=False)
+  app.run(host="127.0.0.1", port=int(os.environ.get('PORT', 5000), debug=False)
